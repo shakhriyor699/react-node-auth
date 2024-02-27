b55b338f58a282b9d9c89859d6b7fb0b53bc5469
shakhriyor348 <62033235+shakhriyor348@users.noreply.github.com>
2024-02-27 22:44:23 +0500

    start

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    app.listen(PORT, () => {
+      console.log('Server is running on port 5000');
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
---
+++ server/models/token-model.js
@@ -0,0 +1,15 @@
+import {Schema, model} from 'mongoose'
+
+
+const tokenSchema = new Schema({
+  refreshToken: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+module.exports = model('Token', tokenSchema)
